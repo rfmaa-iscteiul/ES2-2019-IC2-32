9c456ae4c9ec120015ad4549be8d8daf37087684
rfmaa-iscteiul <56076554+rfmaa-iscteiul@users.noreply.github.com>
2020-03-22 21:17:56 +0000

    atualizacao

--- ProjectoES2/src/main/java/HelloWorld.java
+++ ProjectoES2/src/main/java/HelloWorld.java
@@ -1,6 +1,13 @@
 
 public class HelloWorld {
 
+	/**
+	 * 
+	 * @author Marcio Jesus, Tiago Sil, Rúben Almeida, Guilherme Almeida, António Godinho, João Ferro 
+	 * @version 1
+	 *
+	 */
+	
 	public String print() {
 		String s = "Hello world this is ES group 32!!";
 		
